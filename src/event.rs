ea830e349fd45a1188a38be74692bbf7fe3e2aeb
ovaphlow <ovaphlow@live.com>
2024-01-03 14:16:25 +0800

    ..

--- src/event.rs
+++ src/event.rs
@@ -3,20 +3,44 @@ use serde::Serialize;
 use sqlx::prelude::*;
 
 use crate::AppState;
+use crate::shared_repository::schemas;
+
+#[derive(Serialize)]
+struct Event {
+    id: i64,
+    #[serde(rename = "relationId")]
+    relation_id: i64,
+    #[serde(rename = "referenceId")]
+    reference_id: i64,
+    tags: String,
+    detail: String,
+    time: String,
+    _id: String,
+    #[serde(rename = "_relationId")]
+    _relation_id: String,
+    #[serde(rename = "_referenceId")]
+    _reference_id: String,
+}
 
 pub async fn get_event(app_data: web::Data<AppState>, req: HttpRequest) -> impl Responder {
     print!("{} {}\n", req.method().to_string(), req.uri().to_string());
 
+    let columns: String = schemas.iter().find(|(table, _)| table == &"events").unwrap().1.join(", ");
+    println!(columns);
+    // let q: String = r#"
+    // select {}
+    // "#;
+
     let result: Result<Vec<sqlx::mysql::MySqlRow>, sqlx::Error> = sqlx::query(
         r#"
-            select id
-                , relation_id
-                , reference_id
-                , json_unquote(tags) tags
-                , json_unquote(detail) detail
-                , date_format(time, '%Y-%m-%d %H:%i:%s') time
-            from events
-            "#,
+        select id
+            , relation_id
+            , reference_id
+            , json_unquote(tags) tags
+            , json_unquote(detail) detail
+            , date_format(time, '%Y-%m-%d %H:%i:%s') time
+        from events
+        "#,
     )
     .fetch_all(&app_data.db)
     .await;
@@ -46,21 +70,4 @@ pub async fn get_event(app_data: web::Data<AppState>, req: HttpRequest) -> impl 
         }
         Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
-}
-
-#[derive(Serialize)]
-struct Event {
-    id: i64,
-    #[serde(rename = "relationId")]
-    relation_id: i64,
-    #[serde(rename = "referenceId")]
-    reference_id: i64,
-    tags: String,
-    detail: String,
-    time: String,
-    _id: String,
-    #[serde(rename = "_relationId")]
-    _relation_id: String,
-    #[serde(rename = "_referenceId")]
-    _reference_id: String,
 }
